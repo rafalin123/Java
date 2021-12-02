8dd15ea1e0a2ec18985e6ee4bfe4adeeba0ce266
rafa <rafakun123@gmail.com>
2021-12-02 16:15:41 +0100

    EjerciciosSegun

---
+++ ejercicios_segun/src/ejercicios_segun/ejercicio1.java
@@ -0,0 +1,14 @@
+package ejercicios_segun;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		
+	}
+
+}
